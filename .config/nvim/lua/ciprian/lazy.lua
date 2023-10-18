bb52f565116a9441cfcef354043598440b647db8
Ionut Ciprian Anescu <ionutcipriananescu@gmail.com>
2023-10-18 20:10:15 +0300

    Update lazy.lua

--- .config/nvim/lua/ciprian/lazy.lua
+++ .config/nvim/lua/ciprian/lazy.lua
@@ -1,7 +1,4 @@
--- Path for lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
--- Check if lazy.nvim is not installed, and install it if not found
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
     "git",
@@ -12,17 +9,11 @@ if not vim.loop.fs_stat(lazypath) then
     lazypath,
   })
 end
-
--- Prepend the lazypath to the runtimepath
 vim.opt.rtp:prepend(lazypath)
 
--- Configure lazy.nvim
-require("lazy").setup({
-  { import = "ciprian.plugins" },
-  { import = "ciprian.plugins.lsp" },
-}, {
+require("lazy").setup({ { import = "ciprian.plugins" }, { import = "ciprian.plugins.lsp" } }, {
   install = {
-    colorscheme = { "catppuccin" },
+    colorscheme = { "nightfly" },
   },
   checker = {
     enabled = true,
@@ -32,28 +23,3 @@ require("lazy").setup({
     notify = false,
   },
 })
-
--- Install 'vim-floaterm' using a plugin manager
-vim.cmd("Plug 'voldikss/vim-floaterm'")
-
--- Configure vim-floaterm for lazygit
-vim.g.floaterm_keymap_new = "<F1>"
-vim.g.floaterm_keymap_next = "<F2>"
-vim.g.floaterm_keymap_prev = "<F3>"
-vim.g.floaterm_keymap_toggle = "<F4>"
-vim.g.floaterm_keymap_kill = "<F12>"
-
--- Define the command to open lazygit in floaterm
-vim.cmd([[
-  autocmd FileType lazygit nnoremap <buffer> q :q<CR>
-  autocmd FileType lazygit nnoremap <buffer> <C-s> :call LazygitToggleStaging()<CR>
-  autocmd FileType lazygit nnoremap <buffer> <C-c> :call LazygitClearHunk()<CR>
-]])
-
--- Function to open lazygit
-function OpenLazygit()
-  vim.cmd("FloatermNew --wintype=split --width=0.9 --height=0.9 lazygit")
-end
-
--- Map a key to open lazygit using the defined function
-vim.api.nvim_set_keymap("n", "<Leader>lg", ":lua OpenLazygit()<CR>", { noremap = true, silent = true
