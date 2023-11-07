c1495c39f357cafdb55b0dd4a197f571b4545814
Ionut Ciprian Anescu <ionutcipriananescu@gmail.com>
2023-11-07 21:21:15 +0200

    Update gitsigns.lua

--- .config/nvim/lua/ciprian/plugins/gitsigns.lua
+++ .config/nvim/lua/ciprian/plugins/gitsigns.lua
@@ -1,5 +1,23 @@
 return {
-  "lewis6991/gitsigns.nvim",
+  "lewis6991/gitsigns.nvim", -- Plugin name
+
+  -- Events that trigger the plugin (BufReadPre and BufNewFile)
   event = { "BufReadPre", "BufNewFile" },
-  config = true,
+
+  config = function()
+    -- Configuration for the gitsigns.nvim plugin
+    require("gitsigns").setup({
+      -- Your plugin configuration goes here
+      -- For example:
+      signs = {
+        add = { hl = "GitSignsAdd", text = "▎" },
+        change = { hl = "GitSignsChange", text = "▎" },
+        delete = { hl = "GitSignsDelete", text = "_▎" },
+        topdelete = { hl = "GitSignsDelete", text = "‾▎" },
+      },
+      numhl = false,
+      linehl = false,
+      keymaps = {},
+    })
+  end,
 }
