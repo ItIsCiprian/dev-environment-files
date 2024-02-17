c17c6e1abfb53db946cf6c18f3885d20556e182e
ItIsCiprian <ionutcipriananescu@gmail.com>
2024-02-17 18:29:41 +0200

    Update comment.lua

--- .config/nvim/lua/ciprian/plugins/comment.lua
+++ .config/nvim/lua/ciprian/plugins/comment.lua
@@ -1,37 +1,19 @@
 return {
-
-  {
-    "catppuccin/nvim",
-    config = function()
-      require("catppuccin").setup({
-        integrations = {
-          cmp = true,
-          gitsigns = true,
-          harpoon = true,
-          illuminate = true,
-          indent_blankline = {
-            enabled = false,
-            scope_color = "sapphire",
-            colored_indent_levels = false,
-          },
-          mason = true,
-          native_lsp = { enabled = true },
-          notify = true,
-          nvimtree = true,
-          neotree = true,
-          symbols_outline = true,
-          telescope = true,
-          treesitter = true,
-          treesitter_context = true,
-        },
-      })
-
-      vim.cmd.colorscheme("catppuccin-macchiato")
-
-      -- Hide all semantic highlights until upstream issues are resolved (https://github.com/catppuccin/nvim/issues/480)
-      for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
-        vim.api.nvim_set_hl(0, group, {})
-      end
-    end,
+  "numToStr/Comment.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "JoosepAlviste/nvim-ts-context-commentstring",
   },
+  config = function()
+    -- import comment plugin safely
+    local comment = require("Comment")
+
+    local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+
+    -- enable comment
+    comment.setup({
+      -- for commenting tsx and jsx files
+      pre_hook = ts_context_commentstring.create_pre_hook(),
+    })
+  end,
 }
