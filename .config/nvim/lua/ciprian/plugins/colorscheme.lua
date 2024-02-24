20eb5513b1fd1903450438bc2c9017dbb9ff2657
ItIsCiprian <ionutcipriananescu@gmail.com>
2024-02-24 22:43:17 +0200

    Update colorscheme.lua

--- .config/nvim/lua/ciprian/plugins/colorscheme.lua
+++ .config/nvim/lua/ciprian/plugins/colorscheme.lua
@@ -1,34 +1,38 @@
 return {
-
   {
-    "catppuccin/nvim",
+    "catppuccin/nvim", -- Plugin identifier for Catppuccin theme for Neovim
     config = function()
+      -- Setup function to configure the Catppuccin theme
       require("catppuccin").setup({
         integrations = {
-          cmp = true,
-          gitsigns = true,
-          harpoon = true,
-          illuminate = true,
+          -- Enabling various integrations with other plugins and features
+          cmp = true, -- Completion plugin integration
+          gitsigns = true, -- Git integration for showing signs in the gutter
+          harpoon = true, -- Project navigation tool
+          illuminate = true, -- Highlight other uses of the current word under the cursor
           indent_blankline = {
-            enabled = false,
-            scope_color = "sapphire",
-            colored_indent_levels = false,
+            enabled = false, -- Disabling indentation guides by default
+            scope_color = "sapphire", -- Color for the scope indicator
+            colored_indent_levels = false, -- Disable coloring indent levels differently
           },
-          mason = true,
-          native_lsp = { enabled = true },
-          notify = true,
-          nvimtree = true,
-          neotree = true,
-          symbols_outline = true,
-          telescope = true,
-          treesitter = true,
-          treesitter_context = true,
+          mason = true, -- Automatically manage external editor tooling
+          native_lsp = { enabled = true }, -- Enable native LSP client integration
+          notify = true, -- Notification manager integration
+          nvimtree = true, -- File explorer integration
+          neotree = true, -- Another file explorer integration, alternative to NvimTree
+          symbols_outline = true, -- Symbols outline for easy navigation
+          telescope = true, -- Fuzzy finder integration
+          treesitter = true, -- Syntax highlighting via Treesitter
+          treesitter_context = true, -- Show code context based on Treesitter
         },
       })
 
+      -- Setting the colorscheme to Catppuccin Macchiato
       vim.cmd.colorscheme("catppuccin-macchiato")
 
-      -- Hide all semantic highlights until upstream issues are resolved (https://github.com/catppuccin/nvim/issues/480)
+      -- Workaround for a known issue with LSP highlights
+      -- Temporarily hide all semantic highlights until the issue is resolved
+      -- More details at https://github.com/catppuccin/nvim/issues/480
       for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
         vim.api.nvim_set_hl(0, group, {})
       end
