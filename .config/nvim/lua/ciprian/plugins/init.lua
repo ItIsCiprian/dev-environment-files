c3edb3824ba80a977827119a427c8ad8bdc28ab8
ItIsCiprian <ionutcipriananescu@gmail.com>
2024-03-29 22:24:02 +0200

    Update init.lua

--- .config/nvim/lua/ciprian/plugins/init.lua
+++ .config/nvim/lua/ciprian/plugins/init.lua
@@ -1,9 +1,38 @@
+-- Plugin configuration for Neovim
+-- This configuration sets up several plugins to enhance the Neovim experience,
+-- focusing on utility, navigation, and editing enhancements.
+
 return {
-  "nvim-lua/plenary.nvim", -- lua functions that many plugins use
+  -- Utility Plugins
+  -- `nvim-lua/plenary.nvim` provides common Lua functions used by many plugins.
+  { "nvim-lua/plenary.nvim" },
 
-  "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+  -- Navigation Plugins
+  {
+    -- `vim-tmux-navigator` allows seamless navigation between tmux panes and Neovim splits.
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    -- Key mappings for tmux navigation: use Ctrl + [h/j/k/l] to navigate splits or tmux panes.
+    -- Ctrl + \ to navigate to the previous pane or split.
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },      -- Left
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },      -- Down
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },        -- Up
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },     -- Right
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" }, -- Previous
+    },
+  },
 
-  "inkarkat/vim-ReplaceWithRegister", -- replace with register contents using motion (gr + motion)
+  -- Editing Enhancements
+  -- `vim-ReplaceWithRegister` offers an efficient way to replace text using the contents of a register.
+  { "inkarkat/vim-ReplaceWithRegister" },
 
-  "github/copilot.vim",
+  -- `copilot.vim` introduces GitHub Copilot support for Neovim, providing AI-based code completions.
+  { "github/copilot.vim" },
 }
