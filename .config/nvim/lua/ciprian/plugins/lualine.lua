4bd835ec87c11621fe4821dd8e96953ef08c1c73
ItIsCiprian <ionutcipriananescu@gmail.com>
2024-03-30 21:55:29 +0200

    Update lualine.lua

--- .config/nvim/lua/ciprian/plugins/lualine.lua
+++ .config/nvim/lua/ciprian/plugins/lualine.lua
@@ -1,21 +1,27 @@
+-- Configuration for the lualine status line in Neovim.
+-- Includes custom theming and additional functionality for displaying pending updates from 'lazy.status'.
 return {
   "nvim-lualine/lualine.nvim",
+  -- Dependency on 'nvim-tree/nvim-web-devicons' for file icons.
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     local lualine = require("lualine")
-    local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+    -- Integration with 'lazy.status' for displaying lazy.nvim update notifications.
+    local lazy_status = require("lazy.status")
 
+    -- Define a custom color palette for the lualine theme.
     local colors = {
       blue = "#65D1FF",
       green = "#3EFFDC",
       violet = "#FF61EF",
       yellow = "#FFDA7B",
       red = "#FF4A4A",
-      fg = "#c3ccdc",
-      bg = "#112638",
-      inactive_bg = "#2c3043",
+      fg = "#c3ccdc",  -- Foreground color
+      bg = "#112638",  -- Background color
+      inactive_bg = "#2c3043",  -- Background color for inactive sections
     }
 
+    -- Custom theme configuration using the defined color palette.
     local my_lualine_theme = {
       normal = {
         a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
@@ -43,13 +49,13 @@ return {
         c = { bg = colors.bg, fg = colors.fg },
       },
       inactive = {
-        a = { bg = colors.inactive_bg, fg = colors.semilightgray, gui = "bold" },
-        b = { bg = colors.inactive_bg, fg = colors.semilightgray },
-        c = { bg = colors.inactive_bg, fg = colors.semilightgray },
+        a = { bg = colors.inactive_bg, fg = colors.fg, gui = "bold" },
+        b = { bg = colors.inactive_bg, fg = colors.fg },
+        c = { bg = colors.inactive_bg, fg = colors.fg },
       },
     }
 
-    -- configure lualine with modified theme
+    -- Setup lualine with the custom theme and configuration for displaying additional information.
     lualine.setup({
       options = {
         theme = my_lualine_theme,
@@ -57,13 +63,16 @@ return {
       sections = {
         lualine_x = {
           {
-            lazy_status.updates,
+            "function",
+            function()
+              return lazy_status.updates()
+            end,
             cond = lazy_status.has_updates,
-            color = { fg = "#ff9e64" },
+            color = { fg = "#ff9e64" },  -- Highlight color for update notifications.
           },
-          { "encoding" },
-          { "fileformat" },
-          { "filetype" },
+          { "encoding" },  -- Display file encoding.
+          { "fileformat" },  -- Display file format (e.g., unix, dos).
+          { "filetype" },  -- Display the filetype of the current buffer.
         },
       },
     })
