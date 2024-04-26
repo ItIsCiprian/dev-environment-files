1c22dd097e4f79deb83cf13d2d4b5613dac127d1
ItIsCiprian <ionutcipriananescu@gmail.com>
2024-04-26 22:37:10 +0300

    Update vim-maximizer.lua

--- .config/nvim/lua/ciprian/plugins/vim-maximizer.lua
+++ .config/nvim/lua/ciprian/plugins/vim-maximizer.lua
@@ -1,6 +1,14 @@
+-- Vim plugin for maximizing and minimizing a split window
 return {
-  "szw/vim-maximizer",
+  "szw/vim-maximizer",  -- The plugin identifier for vim-maximizer
+
+  -- Key bindings for the plugin
   keys = {
-    { "<leader>sm", "<cmd>MaximizerToggle<CR>", { desc = "Maximize/minimize a split" } },
+    -- Leader key followed by 'sm' to toggle split window maximization
+    {
+      "<leader>sm",  -- The key combination to trigger the command
+      "<cmd>MaximizerToggle<CR>",  -- The Vim command to maximize or minimize the current split
+      { desc = "Maximize/minimize a split" }  -- Description for the key mapping
+    },
   },
 }
