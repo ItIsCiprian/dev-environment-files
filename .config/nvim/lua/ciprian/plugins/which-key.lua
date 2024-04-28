2883b805bd793331962822bdecaf0020b4ac757a
ItIsCiprian <ionutcipriananescu@gmail.com>
2024-04-28 22:45:09 +0300

    Update which-key.lua

--- .config/nvim/lua/ciprian/plugins/which-key.lua
+++ .config/nvim/lua/ciprian/plugins/which-key.lua
@@ -1,13 +1,21 @@
+-- Plugin configuration for 'folke/which-key.nvim'.
+-- This plugin is loaded with a custom event labeled "VeryLazy".
+-- Configuration applies to initializing settings specific to Neovim's handling of key timeouts.
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
+  "folke/which-key.nvim",  -- Specifies the GitHub repository of the plugin.
+  
+  event = "VeryLazy",  -- Custom event name used to defer loading the plugin.
+  
+  -- Initializes settings immediately before the plugin is loaded.
   init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 500
+    vim.o.timeout = true  -- Enables Neovim's option to detect key sequence timeout.
+    vim.o.timeoutlen = 500  -- Sets the length of time to wait for a mapped sequence to complete (in milliseconds).
   end,
+  
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+    -- Configuration options for 'which-key.nvim'.
+    -- Place your custom settings within this table.
+    -- If left empty, default settings of the plugin are used.
+    -- Refer to the official 'which-key.nvim' documentation for configuration details.
   },
 }
