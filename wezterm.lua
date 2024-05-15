12fed2f3aae4a693085bebcf395f5063d23f0c4b
ItIsCiprian <ionutcipriananescu@gmail.com>
2024-05-15 23:16:54 +0300

    Create wezterm.lua

---
+++ wezterm.lua
@@ -0,0 +1,29 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+local custom = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
+custom.background = "#000000"
+custom.tab_bar.background = "#040404"
+custom.tab_bar.inactive_tab.bg_color = "#0f0f0f"
+custom.tab_bar.new_tab.bg_color = "#080808"
+
+config.color_schemes = {
+	["OLEDppuccin"] = custom,
+}
+config.color_scheme = "OLEDppuccin"
+
+config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font_size = 12
+
+config.enable_tab_bar = false
+
+config.window_decorations = "RESIZE"
+
+-- and finally, return the configuration to wezterm
+return config
