2c0d0ef35680f11ae14e3f82e51e308f613c36a3
ItIsCiprian <ionutcipriananescu@gmail.com>
2024-07-17 22:51:24 +0300

    Update colors.sh

--- .config/sketchybar/colors.sh
+++ .config/sketchybar/colors.sh
@@ -1,42 +1,31 @@
 #!/bin/bash
 
-### Sonokai
-# export BLACK=0xff181819
-# export WHITE=0xffe2e2e3
-# export RED=0xfffc5d7c
-# export GREEN=0xff9ed072
-# export BLUE=0xff76cce0
-# export YELLOW=0xffe7c664
-# export ORANGE=0xfff39660
-# export MAGENTA=0xffb39df3
-# export GREY=0xff7f8490
-# export TRANSPARENT=0x00000000
-# export BG0=0xff2c2e34
-# export BG1=0xff363944
-# export BG2=0xff414550
+### Color Scheme Configuration
 
-### Catppuccin Machiato
-export BLACK=0xff24273a
-export WHITE=0xffcad3f5
-export RED=0xffed8796
-export GREEN=0xffa6da95
-export BLUE=0xff8aadf4
-export YELLOW=0xffeed49f
-export ORANGE=0xfff5a97f
-export MAGENTA=0xffc6a0f6
-export GREY=0xff939ab7
-export TRANSPARENT=0x00000000
-export BG0=0xff1e1e2e
-export BG1=0x603c3e4f
-export BG2=0x60494d64
+# Catppuccin Machiato Color Scheme
+export BLACK=0xff24273a       # Background color - Dark
+export WHITE=0xffcad3f5       # Foreground color - Light
+export RED=0xffed8796         # Accent color - Red
+export GREEN=0xffa6da95       # Accent color - Green
+export BLUE=0xff8aadf4        # Accent color - Blue
+export YELLOW=0xffeed49f      # Accent color - Yellow
+export ORANGE=0xfff5a97f      # Accent color - Orange
+export MAGENTA=0xffc6a0f6     # Accent color - Magenta
+export GREY=0xff939ab7        # Neutral color - Grey
+export TRANSPARENT=0x00000000 # Transparent color
 
-# General bar colors
-export BAR_COLOR=$BG0
-export BAR_BORDER_COLOR=$BG2
-export BACKGROUND_1=$BG1
-export BACKGROUND_2=$BG2
-export ICON_COLOR=$WHITE # Color of all icons
-export LABEL_COLOR=$WHITE # Color of all labels
-export POPUP_BACKGROUND_COLOR=$BAR_COLOR
-export POPUP_BORDER_COLOR=$WHITE
-export SHADOW_COLOR=$BLACK
+# Background Gradient Colors
+export BG0=0xff1e1e2e         # Primary background color
+export BG1=0x603c3e4f         # Secondary background color with 60% opacity
+export BG2=0x60494d64         # Tertiary background color with 60% opacity
+
+### General Bar Colors
+export BAR_COLOR=$BG0             # Bar background color
+export BAR_BORDER_COLOR=$BG2      # Bar border color
+export BACKGROUND_1=$BG1          # First background gradient
+export BACKGROUND_2=$BG2          # Second background gradient
+export ICON_COLOR=$WHITE          # Icon color
+export LABEL_COLOR=$WHITE         # Label text color
+export POPUP_BACKGROUND_COLOR=$BAR_COLOR # Popup background color
+export POPUP_BORDER_COLOR=$WHITE  # Popup border color
+export SHADOW_COLOR=$BLACK        # Shadow color
